cb2ae0a21e19a22c1b1242785ec06ac31ac3e4c6
Vito <vitobernal@hotmail.com>
2026-09-15 12:12:14 -0300

    Creación y estructura inicial del proyecto

---
+++ TrabajoPractico5/src/main/java/ar/edu/unju/fi/poo/TrabajoPractico5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoPractico5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoPractico5Application.class, args);
+	}
+
+}
